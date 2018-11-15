6ecaa8f1dec90b6e2f095dfede5cb2b842daae3e
Domitrius Clark <domitriusaclark@gmail.com>
2018-11-14 22:22:34 -0500

    Added comments to main index file.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import { client } from './ApolloLinkState/apollo';
 import './index.css';
 import App from './ApolloLinkState/App';
 
+// Uncomment this for the context app 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
-
+// Comment this out if you uncomment above
 const WrappedApp = (
     <ApolloProvider client={client}>
         <App />
