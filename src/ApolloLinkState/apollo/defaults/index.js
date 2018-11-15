8420a206a768ce336725a6aa0016aa887aecf4c6
Domitrius Clark <domitriusaclark@gmail.com>
2018-11-14 22:10:59 -0500

    Added a mini project demonstrating how to use Apollo-link-state to handle local state manipulation and storage.

---
+++ src/ApolloLinkState/apollo/defaults/index.js
@@ -0,0 +1,10 @@
+// This is Apollos way of setting the default state
+
+export default {
+    apolloClientDemo: {
+        // This is the default name of the identifier our cache uses
+        __typename: 'ApolloClientDemo',
+        // the specific item of data that our header will use to display our current page name (with the default state of it set to 'Apollo Demo')
+        currentPageName: 'Apollo Demo'
+    }
+};
---
+++ src/ApolloLinkState/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import { getPageNameQuery } from '../graphql';
+
+const Header = props => (
+    <Query query={getPageNameQuery}>
+        {({ loading, error, data}) => {
+            if(error) return <h1>Error...</h1>;
+            if (loading || !data) return <h1>Loading...</h1>
+
+            return <h1>{data.apolloClientDemo.currentPageName}</h1>
+        }}
+    </Query>
+);
+
+export default Header;
